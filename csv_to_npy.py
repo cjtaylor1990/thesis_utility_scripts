c9c5cefea8f6b690fd3b2bbbf422ab51697fe9ff
cjtaylor1990 <cjtaylor2390@gmail.com>
2020-12-03 02:05:21 -0500

    Renamed convert_txt_to_npy to csv_to_npy. I also  did some significant refactoring of the scripts.

--- csv_to_npy.py
+++ csv_to_npy.py
@@ -2,35 +2,34 @@ import numpy as np
 import csv
 import sys
 
-def read_txt_to_npy(inputFile, delimiter=' '):
+def read_csv_to_npy(inputFile, group_by_column=False, delimiter=' '):
     """
-    This function was created to read in CSV .txt files. It
-    assumes that all rows in the file have the same number of
-    columns. The output is a numpy array that is grouped by
-    column.
+    This function was created to read in the common CSV text files
+    that other scripts produce as output. The CSV is assumed to be uniform,
+    that is each row has the same number of columns.
 
     Required Variables:
     inputFile = string that contains path to file that will be read
-    
+
     Optional Variables:
     delimiter = string with character that seperates column in CSV
+    group_by_column = if true, this will transpose the input, grouping by column
 
-    Example:
+    Example 1:
     ./inputFile.txt
     0 0 0
     1 1 1
 
-    convert_txt_to_npy('./inputFile.txt')
-    => numpy.array([0, 1], [0, 1], [0, 1])
+    read_csv_to_npy('./inputFile.txt') => numpy.array([["0", "0", "0"], ["1", "1", "1"]])
+    
+    read_csv_to_npy('./inputFile.txt', group_by_column=True) => numpy.array([["0", "1"], ["0", "1"], ["0", "1"]])
+
     """
     data = csv.reader(open(inputFile, 'r'), delimiter=delimiter)
-
     output = np.array([row for row in data])
-    output = np.transpose(output)
+    return np.transpose(output) if (group_by_column) else output
 
-    return output
-
-def convert_txt_to_npy(inputFile, outputFile, delimiter=' '):
+def convert_csv_to_npy(inputFile, outputFile, group_by_column=False, delimiter=' '):
     """
     This function was created to smoothly convert CSV .txt files that
     are the outputs of other scripts and convert them to easily manipulated
@@ -54,12 +53,15 @@ def convert_txt_to_npy(inputFile, outputFile, delimiter=' '):
     ./outputFile.npy
     numpy.array([0, 1], [0, 1], [0, 1])
     """
-    np.save(outputFile, read_txt_to_npy(inputFile))
-
+    np.save(outputFile, read_csv_to_npy(inputFile, group_by_column=group_by_column, delimiter=delimiter))
 
 if __name__=="__main__":
     inputFile = sys.argv[1]
     outputFile = sys.argv[2]
+    if len(sys.argv) > 3 and sys.argv[3].lower() == "true":
+        group_by_column = True
+    else:
+        group_by_column = False
 
-    convert_txt_to_npy(inputFile, outputFile)
+    convert_csv_to_npy(inputFile, outputFile, group_by_column=group_by_column)
 
