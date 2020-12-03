d6cdd231735fbaf2aec69354e12b6633a861bbee
cjtaylor1990 <cjtaylor2390@gmail.com>
2020-12-03 02:07:11 -0500

    Created a mass-scale csv-to-npy script that uses the other two scripts as dependencies.

---
+++ mass_csv_to_npy.py
@@ -0,0 +1,14 @@
+import sys
+from automate_script import automate_script
+import csv_to_npy
+
+inputFile = sys.argv[1]
+csvList = csv_to_npy.read_csv_to_npy(inputFile)
+
+def convert_file(csv_file):
+    npy_file = str(csv_file[0])[:-3] + 'npy'
+    csv_to_npy.convert_csv_to_npy(csv_file[0], npy_file)
+
+if __name__ == '__main__':
+    inputFile = sys.argv[1]
+    automate_script(convert_file, inputFile)
