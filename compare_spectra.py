a7bdc41bbc4a407862f3a0f03cb74b882dc0e416
cjtaylor1990 <cjtaylor2390@gmail.com>
2020-12-15 03:08:54 -0500

    Adding title optional parameter to compare_spectra. Adding .DS_Store to .gitignore.

--- compare_spectra.py
+++ compare_spectra.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as pl
 import sys
 from plot_spectrum import load_spectral_data, default_spectrum_layout, setup_spectrum_layout, save_and_clear_spectrum
 
-def compare_spectra(test_file, control_file, saveFile=''):
+def compare_spectra(test_file, control_file, saveFile='', title=''):
     test_energy, test_value = load_spectral_data(sys.argv[1], 0, 2, grouped_by_column=False)
     control_energy, control_value = load_spectral_data(sys.argv[2], 0, 1, grouped_by_column=True)
 
@@ -14,9 +14,11 @@ def compare_spectra(test_file, control_file, saveFile=''):
     pl.yscale('log')
     pl.ylabel(r'$E^{2}\times$Photon Flux')
     pl.xlabel(r'$E$ (keV)')
+    if title:
+        pl.title(title)
 
     if saveFile:
         save_and_clear_spectrum(saveFile)
 
 if __name__ == "__main__":
-    compare_spectra(sys.argv[1], sys.argv[2], saveFile=sys.argv[3])
+    compare_spectra(sys.argv[1], sys.argv[2])
